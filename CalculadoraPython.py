48ef0380e6133ac2964315fffc4fdaa09d9c2189
HeroPX1223 <moqui.mer09@gmail.com>
2025-10-06 11:47:21 -0400

    Add files via upload

---
+++ CalculadoraPython.py
@@ -0,0 +1,14 @@
+Numero1 = int(input("Cual es el primer numero?: "))
+Numero2 = int(input("Cual es el segundo numero?: "))
+
+suma = Numero1 + Numero2
+resta = Numero1 - Numero2
+multiplicacion = Numero1 * Numero2
+division = Numero1 / Numero2
+potencia = Numero1 ** Numero2 
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)
---
+++ NombrePython.py
@@ -0,0 +1,4 @@
+nombre_usuario = "Fernando Reverol"
+edad = 16
+print("El nombre de usuario es " + nombre_usuario)
+print(f"El nombre de usuario es {nombre_usuario} y tengo {edad} años")
