616315d17f9fa12364cfaf44f149a146a76d5a4d
HeroPX1223 <moqui.mer09@gmail.com>
2025-10-07 11:51:56 -0400

    Add files via upload

---
+++ DiccionaroFernan.py
@@ -0,0 +1,9 @@
+diccionario_fernan = {
+    "Nombre": "Fernando",
+    "Edad": 16,
+    "Fecha de Nacimiento": "23 de diciembre de 2008",
+    "Deporte favorito": "Fútbol",
+    "Videojuego favorito": "Call of Duty"
+}    
+for clave, valor in diccionario_fernan.items():
+        print (f"\nClave: {clave}, Valor: {valor}")
---
+++ ListaCompañeros.py
@@ -0,0 +1,7 @@
+lista_chavales = ["Moises Perez", "Nicole Gutierrez", "Heberto Melendez", 
+                  "Orlaine Rodriguez", "Ema Eguis", "Xavier Lidueña", 
+                  "Jesus Hernandez", "Jose Carrasquero", "Samuel Alvarado", 
+                  "Fernando Reverol", "Osman Ruiz", "Raul Hernandez", "Jose Iguaran"]
+
+for nombre in lista_chavales:
+    print(f"Hello, {nombre}")
