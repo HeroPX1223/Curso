a7f11507dbf2bdba0fb6dae681f025eb31cf1164
HeroPX1223 <moqui.mer09@gmail.com>
2025-10-07 09:29:51 -0400

    Add files via upload

---
+++ Calificaciones.py
@@ -0,0 +1,14 @@
+Calificacion = 110
+
+if Calificacion > 90 and Calificacion <= 100:
+    print("A")
+elif Calificacion > 80 and Calificacion < 89:
+    print("B")
+elif Calificacion > 70 and Calificacion < 79:
+    print("C")
+elif Calificacion > 60 and Calificacion < 69:
+    print("D")
+elif Calificacion > 0 and Calificacion < 59:
+    print("F")
+else:
+    print("El número ingresado no es válido")
