93aa8beafe9e416ba1a3fec13673e47abc94df1b
HeroPX1223 <moqui.mer09@gmail.com>
2025-10-15 12:00:39 -0400

    Add files via upload

---
+++ AreaTriangulo.js
@@ -0,0 +1,11 @@
+let base = 10;
+let altura = 5;
+
+
+
+function calcularArea(base, altura) {
+    const area = base * altura / 2;
+    console.log("El área del triángulo es: " + area);
+}
+
+calcularArea(base, altura);
