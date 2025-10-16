93f66df3c53bdd1b79156eee50dfbdc557206fe4
HeroPX1223 <moqui.mer09@gmail.com>
2025-10-16 12:05:08 -0400

    Add files via upload

---
+++ Compañeros.sql
@@ -0,0 +1,22 @@
+CREATE TABLE estudiantes (
+    cedula INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    edad INT,
+)
+INSERT INTO estudiantes (cedula, nombre, apellido, edad) VALUES
+(31722658, 'Nicole', 'Gutierrez', 19),
+(32469083, 'Heberto Luis', 'Melendez Adames', 17),
+(32841060, 'Ema Luna', 'Eguis Bracho', 17),
+(33046892, 'Xavier Antonio', 'Lidueña Galea', 17),
+(33104401, 'Jesus Daniel', 'Hernandez Monsalve', 17),
+(33458666, 'Fernando Eugenio', 'Reverol García', 16),
+(33512164, 'Osman Jose de Jesus', 'Ruiz Layett', 17),
+(30862844, 'Jose Daniel', 'Iguarán Gonzalez', 21); 
+
+
+
+
+
+
+
